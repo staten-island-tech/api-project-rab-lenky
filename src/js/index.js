a04a55043c7fff017442c71022dc9b2d1fb44e21
DanBel23 <daniel23.bel@gmail.com>
2021-05-13 21:25:14 -0400

    Edit 5/13/21

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,16 @@ import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
 const key = "YOURKEYHERE";
+
+const query = async function () {
+  try {
+    const response = await fetch(
+      `https://api.thecatapi.com/v1/images/search?breed_ids={breed-id}`
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+    alert("Hey something went wrong");
+  }
+};
